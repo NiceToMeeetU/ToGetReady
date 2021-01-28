39dbcc69282d16fa70a7cfe315f38dc0fdd52a88
WangYu <nicetomeeetu@foxmail.com>
2021-01-28 22:42:32 +0800

    0128

---
+++ Code/internship/__init__.py
@@ -0,0 +1,6 @@
+# coding   : utf-8 
+# @Time    : 21/01/28 15:48
+# @Author  : Wang Yu
+# @Project : ToGetReady
+# @File    : __init__.py.py
+# @Software: PyCharm
---
+++ Code/internship/alpha.py
@@ -0,0 +1,8 @@
+# coding   : utf-8 
+# @Time    : 21/01/28 15:48
+# @Author  : Wang Yu
+# @Project : ToGetReady
+# @File    : alpha.py
+# @Software: PyCharm
+
+
