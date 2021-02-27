9b614913cd21a3528f1d529ee395e751b9ff86e5
WangYu <nicetomeeetu@foxmail.com>
2021-02-27 20:37:23 +0800

    0227

--- Code/leetcode_everyday/0227.py
+++ Code/leetcode_everyday/0227.py
@@ -75,6 +75,15 @@ class Solution:
         return res
 
 
+    def maxSubArray(self, nums: List[int]) -> int:
+        """
+        剑指offer 42
+        输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+        要求时间复杂度为O(n)。
+        :param nums: [-2,1,-3,4,-1,2,1,-5,4]
+        :return:6
+
+        """
 
 
     def test(self):
