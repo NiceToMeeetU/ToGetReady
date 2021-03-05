c87525b93530544873dd605fe0bdfbf3f318b19a
WangYu <nicetomeeetu@foxmail.com>
2021-03-05 19:40:26 +0800

    0305

--- Code/leetcode_everyday/0305.py
+++ Code/leetcode_everyday/0305.py
@@ -9,12 +9,14 @@
 from typing import List
 import collections
 
+
 class Node:
-    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next_: 'Node' = None):
         self.val = val
         self.left = left
         self.right = right
-        self.next = next
+        self.next = next_
+
 
 class TreeNode:
     def __init__(self, x):
@@ -72,14 +74,65 @@ class Solution:
                 tmp = curNode
         return root
 
-
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         """
+        给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+        百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，
+        最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+        向上回溯找祖先
+        """
+        if not root or root == p or root == q:
+            return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if not left:
+            return right
+        if not right:
+            return left
+        return root
 
-        :param root:
-        :param p:
-        :param q:
+    def reverseList(self, head: ListNode) -> ListNode:
+        """
+        递归反转列表
+        :param head:
         :return:
         """
+        if not head or not head.next:
+            return head
+        res = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return res
+
+    def reverseListToN(self, head: ListNode, n: int):
+        """
+        反转从head为起点的n个节点
+        """
+        nonlocal successor
+        if n == 1:
+            successor = head.next
+            return head
+        res = self.reverseListToN(head.next, n - 1)
+        head.next.next = head
+        head.next = successor
+        return res
+
+    def reverseBetween(self, head: ListNode, left, right):
+        """
+        92 反转从m到n的链表
+        依然是递归的思想
+        """
+        if left == 1:
+            return self.reverseListToN(head, right)
+        head.next = self.reverseBetween(head, left - 1, right - 1)
+        return head
+
+
+class ListNode(object):
+    def __init__(self, val=0, next_=None):
+        self.val = val
+        self.next = next_
+
+
 if __name__ == '__main__':
     pass
