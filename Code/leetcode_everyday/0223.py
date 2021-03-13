228e96ab214d9f8da9a9e3e93f1611cef0cddca5
WangYu <nicetomeeetu@foxmail.com>
2021-03-13 10:02:02 +0800

    0223

--- Code/leetcode_everyday/0223.py
+++ Code/leetcode_everyday/0223.py
@@ -9,7 +9,20 @@ from typing import List
 
 class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], X: int) -> int:
-        """First Try: 太麻烦了，未考虑len=1的特殊情况"""
+        """
+        1052. 爱生气的书店老板
+        今天，书店老板有一家店打算试营业customers.length分钟。
+        每分钟都有一些顾客（customers[i]）会进入书店，所有这些顾客都会在那一分钟结束后离开。
+        在某些时候，书店老板会生气。 如果书店老板在第 i 分钟生气，
+        那么 grumpy[i] = 1，否则 grumpy[i] = 0。
+        当书店老板生气时，那一分钟的顾客就会不满意，不生气则他们是满意的。
+        书店老板知道一个秘密技巧，能抑制自己的情绪，可以让自己连续X 分钟不生气，但却只能使用一次。
+        请你返回这一天营业下来，最多有多少客户能够感到满意的数量。
+        :param customers:[1,0,1,2,1,1,7,5]
+        :param grumpy:[0,1,0,1,0,1,0,1]
+        :param X:3
+        :return:16
+        """
         # total = 0
         # for c, g in zip(customers, grumpy):
         #     total += c if g == 0 else 0
