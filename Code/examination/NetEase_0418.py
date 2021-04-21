7bcd86eed4dbc8fb87fc214885f57ed2fc4d96d0
WangYu <nicetomeeetu@foxmail.com>
2021-04-21 22:00:16 +0800

    0421

--- Code/examination/NetEase_0418.py
+++ Code/examination/NetEase_0418.py
@@ -6,8 +6,6 @@
 # @Software: PyCharm
 
 
-import sys
-
 #
 # class Solution:
 #     def subArray(self, s, t):
@@ -47,14 +45,70 @@ import sys
 #     for i, j in query:
 #         ans = solution.subArray(s[i - 1:], t[j - 1:])
 #         print(ans)
+#
+# s = "abcd"
+# t = "adcb"
+#
+# t_idx = dict()
+# for i,c in enumerate(t):
+#     tmp = ord(c) - ord("a")
+#     if tmp not in t_idx:
+#         t_idx[tmp] = i
+#
+# s = [ord(c) - ord("a") for c in s]
 
-s = "abcd"
-t = "adcb"
 
-t_idx = dict()
-for i,c in enumerate(t):
-    tmp = ord(c) - ord("a")
-    if tmp not in t_idx:
-        t_idx[tmp] = i
+import sys
+import pandas as pd
 
-s = [ord(c) - ord("a") for c in s]
+
+class Solution:
+    def task1(self, df_plays):
+
+
+        return df_plays
+
+if __name__ == '__main__':
+    """
+    ACM 模式需要自己读取测试用例输入。
+    假定本题指定的输入格式如下：
+    
+    第一行输入一个整数n，表示后面有n条数据输入
+    从第二行开始的n行，每一行为一条记录，分别是时间戳，玩家ID，新手节点ID，是否通过，节点用时。
+    各字段用空格隔开
+    """
+
+    n = int(sys.stdin.readline().strip())
+    raw_data = []
+    for _ in range(n):
+        line = sys.stdin.readline().strip().split()
+        raw_data.append(line)
+    test = """11:36:00	1	1001	TRUE	60
+            12:36:00	1	1002	FALSE	30
+            13:36:00	2	1001	TRUE	45
+            14:36:00	2	1002	TRUE	23
+            15:36:00	2	1003	TRUE	25
+            16:36:00	2	1004	FALSE	14
+            17:36:00	3	1001	TRUE	25
+            18:36:00	3	1002	FALSE	36
+            19:36:00	3	1003	FALSE	18
+            20:36:00	3	1004	FALSE	26
+            21:36:00	4	1001	FALSE	35
+            22:36:00	4	1002	FALSE	29"""
+    raw = [line.split() for line in test.split("\n")]
+
+    data = pd.DataFrame(raw, columns=["timestramp", "player_id", "level_id", "pass", "time"])
+    data["pass"] = data["pass"].astype(lambda x: False if x == "False" else True)
+
+
+    data = data.astype({"timestramp": str, "player_id": int, "level_id": int, "pass": lambda x: False if x == "False" else True, "time": float})
+    #
+    # solution = Solution()
+    # ans = solution.task1(data)
+    # print(ans)
+
+
+
+
+
+
